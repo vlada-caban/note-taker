7dfbefea44c8b6a01d549543ee0205f5ef25bfcd
vlada_caban <vladaonly@gmail.com>
2023-06-07 21:57:06 -0400

    added get and post methods

--- server.js
+++ server.js
@@ -1,17 +1,59 @@
 const express = require("express");
 const path = require("path");
 const notes = require("./db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
 const PORT = 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/api/notes", (req, res) => res.json(notes));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => res.status(200).json(notes));
+
+app.post("/api/notes", (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    // const noteString = JSON.stringify(newNote);
+    // console.log(noteString);
+    // let existingNotes = JSON.stringify(notes);
+    // console.log(existingNotes);
+    // existingNotes.push(noteString);
+    // console.log(existingNotes);
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in saving the note");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
