335b45eaba56598ce320a382324a2430c200f204
vlada_caban <vladaonly@gmail.com>
2023-06-10 17:31:41 -0400

    added delete function

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const notes = require("./db/db.json");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,62 +13,92 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
 
+//function to navigate user to notes page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+//function to get notes from database to render on the page
 app.get("/api/notes", (req, res) => res.status(200).json(notes));
 
+//function to add a new note
 app.post("/api/notes", (req, res) => {
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
-    const newNote = {
-      title,
-      text,
-      id: uuidv4(),
-    };
-
-    const response = {
-      status: "success",
-      body: newNote,
-    };
-
-    let savedNotes = notes;
-    // console.log(savedNotes);
-    savedNotes.push(newNote);
-    // console.log(savedNotes);
-
-    fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `New note successfully saved`
-          )
-    );
-
-    // const noteString = JSON.stringify(newNote);
-    // console.log(noteString);
-    // let existingNotes = JSON.stringify(notes);
-    // let arrayOfNotes = eval(existingNotes)
-    // console.log(existingNotes);
-    // console.log(arrayOfNotes);
-
-    // arrayOfNotes.push(noteString);
-    // console.log(arrayOfNotes);
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json("Error in saving the note");
+  if (!title || !text) {
+    return res.status(500).json({msg: "Error in saving the note"});
   }
+  const newNote = {
+    title,
+    text,
+    id: uuidv4(),
+  };
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+
+  notes.push(newNote);
+
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) =>
+    err ? console.error(err) : console.log("New note successfully saved")
+  );
+
+  res.status(201).json(response);
+
+  //old code
+  // Checking if all the required properties are present
+  // if (title && text) {
+  //   const newNote = {
+  //     title,
+  //     text,
+  //     id: uuidv4(),
+  //   };
+
+  //   const response = {
+  //     status: "success",
+  //     body: newNote,
+  //   };
+
+  //   notes.push(newNote);
+
+  //   fs.writeFile("./db/db.json", JSON.stringify(notes), (err) =>
+  //     err ? console.error(err) : console.log("New note successfully saved")
+  //   );
+
+  //   console.log(response);
+  //   res.status(201).json(response);
+  // } else {
+  //   res.status(500).json("Error in saving the note");
+  // }
+});
+
+//function to delete a note
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+
+  const indexToDelete = notes.findIndex((note) => note.id === id);
+  if (indexToDelete === -1) {
+    return res.status(500).json({msg: "Note is not found"});
+  }
+  notes.splice(indexToDelete, 1);
+  console.log(notes);
+
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) =>
+    err ? console.error(err) : console.log("Note deleted")
+  );
+
+  res.status(200).json({ msg: "Successfully deleted" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
